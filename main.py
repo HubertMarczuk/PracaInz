b34790fd7004a4f3f1a4ef683f660be43b683e2e
Hubert Marczuk <dziabongjp@gmail.com>
2023-08-03 01:23:45 +0200

    Quicksort prototype done

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 def quicksort_prot(tab, left, right):
-    print(right)
-    print(left)
-    if right <= left:
+    if right - 1 <= left:
         return
     pivot = tab[int((left + right) / 2)]
     i = left
     j = right
-    k = 0
     while 1:
         while pivot > tab[i]:
             i = i + 1
         while pivot < tab[j]:
             j = j - 1
-        k += 1
-        if i <= j:
-            print(i)
-            print(j)
+        if i < j:
             temp = tab[i]
             tab[i] = tab[j]
             tab[j] = temp
-            print(tab)
         else:
             break
     if j > left:
@@ -29,6 +22,5 @@ def quicksort_prot(tab, left, right):
     return tab
 
 
-print(
-    quicksort_prot([5, 2, 1, 3, 6, 9, 4, 8, 7], 0, len([5, 2, 1, 3, 6, 9, 4, 8, 7]) - 1)
-)
+tab = quicksort_prot([9, 8, 7, 6, 5, 4, 3, 2], 0, 7)
+print(tab)
