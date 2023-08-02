df17034c6c359491b2a8a217c97b93edc00458dd
Hubert Marczuk <dziabongjp@gmail.com>
2023-08-02 19:39:37 +0200

    Quicksort protype in progress

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-for i in range(21,37):
-    print("test")
-    print("drugi")
-print("koniec")
+def quicksort_prot(tab):
+    right = len(tab) - 1
+    left = 0
+    pivot = tab[int((left + right) / 2)]
+    i = left
+    j = right
+    while 1:
+        while pivot > tab[i]:
+            i = i + 1
+        while pivot < tab[j]:
+            j = j - 1
+        if i <= j:
+            temp = tab[i]
+            tab[i] = tab[j]
+            tab[j] = temp
+        else:
+            break
+    if j > left:
+        quicksort_prot(tab, left, j)
+    if j > left:
+        quicksort_prot(tab, i, right)
+    return tab
+
+
+print(quicksort_prot([6, 2, 1, 3, 5, 9, 4, 8, 7]))
